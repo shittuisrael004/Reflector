a0d37bde3d2e10554f96703152279f40d0a322e2
shittuisrael004 <shittuisrael004@gmail.com>
2026-01-07 00:05:30 +0100

    ready to deploy on base sepolia for testing

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 **Reflector is an immediate refund contract on base network that just resends all the ether sent to the contract with small fee.**
 
 
-## Built with Foundry, check documentation below
+### Built with Foundry, check documentation below
 
 https://book.getfoundry.sh/
---
+++ script/Deploy.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {Reflector} from "../src/Reflector.sol";
+
+contract DeployScript is Script {
+    Reflector public reflector;
+
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        reflector = new Reflector();
+
+        vm.stopBroadcast();
+
+        // console.log("Contract deployed to:", address(reflector)); 
+    }
+}
